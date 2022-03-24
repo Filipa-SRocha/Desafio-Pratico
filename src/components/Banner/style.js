b7e9a35d6f7b6571c5d320bbd694eab97ce2b95c
Filipa-SRocha <filipa.sr2@gmail.com>
2022-03-24 19:47:11 +0000

    Ajustei responsividade do Banner

--- src/components/Banner/style.js
+++ src/components/Banner/style.js
@@ -4,26 +4,37 @@ export const Container = styled.div`
 	height: 200px;
 	background: #1b75bc;
 	display: grid;
-	grid-template-columns: 1fr 1fr 1fr 1fr;
+	grid-template-columns: 1fr 1fr;
 	grid-template-rows: 1fr 1fr 1fr 1fr 1fr;
 	grid-template-areas:
-		'. . . .'
-		'. . . .'
-		'. imagem texto .'
-		'. imagem . .'
-		'. imagem . .'; ;
+		' . .'
+		' . .'
+		'imagem texto'
+		'imagem .'
+		'imagem .';
+
+	@media (max-width: 280px) {
+		display: flex;
+		align-items: center;
+	}
 `;
 
-export const FancyText = styled.span`
+export const FancyText = styled.p`
 	grid-area: texto;
 	font-family: 'Kaushan Script', cursive;
 	color: white;
 	font-size: 26px;
 	justify-self: start;
+	@media (max-width: 280px) {
+		text-align: center;
+	}
 `;
 
 export const BannerImage = styled.img`
 	grid-area: imagem;
 	height: 180px;
 	justify-self: end;
+	@media (max-width: 280px) {
+		display: none;
+	}
 `;
