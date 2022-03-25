86243113416ba6b5bc958d3389e0130c1372ce92
Filipa-SRocha <filipa.sr2@gmail.com>
2022-03-25 21:40:51 +0000

    Changed order to Z-A

--- src/routes/fabricante/fabricante.jsx
+++ src/routes/fabricante/fabricante.jsx
@@ -14,7 +14,7 @@ function Fabricante() {
 		fetch(url)
 			.then((response) => response.json())
 			.then((data) => {
-				setModelos(data.modelos);
+				setModelos(data.modelos.reverse());
 			});
 	}, []);
 
